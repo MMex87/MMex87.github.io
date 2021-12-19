7f881519ecb2fb3cc54142f9e82ea74d9a78ad8f
Okky Setya Kurniawan <85876134+MMex87@users.noreply.github.com>
2021-12-19 18:55:12 +0700

    Update dataSpatial.js

--- dataSpatial.js
+++ dataSpatial.js
@@ -2,7 +2,7 @@ function initMap(){
 	
 	var options = {
 		center: {lat: -7.4707422, lng: 112.4198329},
-		zoom: 36
+		zoom: 8
 	}
 	map = new google.maps.Map(document.getElementById("map"),options);
 }
